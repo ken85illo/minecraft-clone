bcfa51d0a0ccb487e61f7991614860b87cf8265b
ken85illo <pabillokenneth81@gmail.com>
2025-07-11 12:50:59 +0800

    chore: change window size

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Window.hpp>
 
 int main() {
-    Window::instance = new Window(1280, 800, "Hello OpenGL!");
+    Window::instance = new Window(960, 720, "Hello OpenGL!");
     Window::instance->mainLoop();
     delete Window::instance;
 }
