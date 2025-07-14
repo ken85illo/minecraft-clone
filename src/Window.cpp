ac5d3ec8a723ccc2af63b9c34fa67df22a21fe58
ken85illo <pabillokenneth81@gmail.com>
2025-07-14 15:02:11 +0800

    refactor: implement newly made classes

--- src/Window.cpp
+++ src/Window.cpp
@@ -1,11 +1,16 @@
 #include "Window.hpp"
+#include <random>
 
 Window* Window::instance = nullptr;
-
+uint16_t Window::m_width = 0;
+uint16_t Window::m_height = 0;
 
 Window::Window(uint16_t width, uint16_t height, const char* title)
-: m_width(width), m_height(height), m_title(title) {
+: m_title(title), m_deltaTime(0.0f) {
+    m_width = width;
+    m_height = height;
 
+    srand(static_cast<unsigned>(time(0)));
     if(!glfwInit()) {
         std::println("Failed to initialize GLFW!");
         return;
@@ -14,6 +19,7 @@ Window::Window(uint16_t width, uint16_t height, const char* title)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, false);
 
     initWindow();
 
@@ -23,25 +29,40 @@ Window::Window(uint16_t width, uint16_t height, const char* title)
 }
 
 Window::~Window() {
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
-    delete tex;
-    delete shader;
+    delete Camera::instance;
+    delete m_shader;
+    delete cubes;
     glfwTerminate();
 }
 
 void Window::initWindow() {
     m_window = glfwCreateWindow(m_width, m_height, m_title, nullptr, nullptr);
+    Camera::instance = new Camera(3.0f, 0.1f, &m_width, &m_height, &m_deltaTime);
     if(!m_window) {
         std::println("Failed to initialize window!");
         return;
     }
     glfwMakeContextCurrent(m_window);
     glfwSetFramebufferSizeCallback(m_window, frameBufferSizeCallback);
+    glfwSetInputMode(m_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPosCallback(m_window, Camera::instance->getCallback());
 
-    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::println("Failed to initialize GLAD!");
+        return;
+    }
+
+    glEnable(GL_DEPTH_TEST);
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    m_shader = new Shader("../src/shaders/shader.vert", "../src/shaders/shader.frag");
+
+    glm::mat4 projection;
+    projection = glm::perspective(glm::radians(45.0f), 1280.0f / 720.0f, 0.1f, 100.0f);
+
+    m_shader->use();
+    m_shader->setMat4("projection", projection);
+    m_shader->setInt("texture0", 0);
 }
 
 
@@ -49,96 +70,62 @@ void Window::mainLoop() {
     if(!m_window)
         return;
 
-    shader = new Shader("../src/shaders/shader.vert", "../src/shaders/shader.frag");
+    float max_x = 20.0f, min_x = -20.0f;
+    float max_y = 20.0f, min_y = -20.0f;
+    float max_z = 20.0f, min_z = -20.0f;
+    cubes = new Cube(500, m_shader, "../res/nerd_hd.jpg");
+    for(int i = 0; i < cubes->getSize(); i++) {
+        float x = min_x +
+        static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (max_x - min_x)));
+        float y = min_y +
+        static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (max_y - min_y)));
+        float z = min_z +
+        static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (max_z - min_z)));
 
-    float vertices[] = {
-        // rectangle
-        // -0.5f, -0.5f, 0.0f, // bottom-left
-        // -0.5f, 0.5f, 0.0f,  // top-left
-        // 0.5f, 0.5f, 0.0f,   // top-right
-        // 0.5f, -0.5f, 0.0f,  // bottom-right
+        std::println("{} {} {}", x, y, z);
+        cubes->setPos(i, x, y, z);
+    }
 
-        // vertex           //color         //tex coords
-        -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, // bottom-left
-        -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 2.0f,  // top-left
-        0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 2.0f, 2.0f,   // top-right
-        0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 2.0f, 0.0f,  // bottom-right
-    };
-
-    uint indices[] = {
-        0, 1, 2, // first triangle
-        0, 2, 3, // second triangle
-    };
-
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(
-    1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-    glVertexAttribPointer(
-    2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(6 * sizeof(float)));
-    glEnableVertexAttribArray(2);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-
-    tex = new Texture(GL_TEXTURE_2D, 2);
-    tex->setParameter(0, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    tex->setParameter(0, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    tex->setParameter(0, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    tex->setParameter(0, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    tex->loadImage(0, "../res/brick.jpg");
-
-    tex->setParameter(1, GL_TEXTURE_WRAP_R, GL_REPEAT);
-    tex->setParameter(1, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    tex->setParameter(1, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    tex->setParameter(1, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    tex->loadImage(1, "../res/dog.jpg");
-
-    shader->use();
-    shader->setInt("texture0", 0);
-    shader->setInt("texture1", 1);
-    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
+    float lastFrame = 0.0f;
     while(!glfwWindowShouldClose(m_window)) {
         update();
         render();
 
         glfwPollEvents();
         glfwSwapBuffers(m_window);
+
+        float currentFrame = glfwGetTime();
+        m_deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
     }
-}
-
-void Window::render() {
-    glClearColor(0.0f, 0.0f, 0.0f, 1.f);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    shader->use();
-    glBindVertexArray(VAO);
-    tex->use(0);
-    tex->use(1);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
-    glBindVertexArray(0);
 }
 
 void Window::update() {
-    if(glfwGetKey(m_window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    if(glfwGetKey(m_window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(m_window, true);
-    }
+    if(glfwGetKey(m_window, GLFW_KEY_W) == GLFW_PRESS)
+        Camera::instance->moveFront();
+    if(glfwGetKey(m_window, GLFW_KEY_S) == GLFW_PRESS)
+        Camera::instance->moveBack();
+    if(glfwGetKey(m_window, GLFW_KEY_D) == GLFW_PRESS)
+        Camera::instance->moveRight();
+    if(glfwGetKey(m_window, GLFW_KEY_A) == GLFW_PRESS)
+        Camera::instance->moveLeft();
+
+    glm::mat4 view = Camera::getViewMat4();
+    m_shader->use();
+    m_shader->setMat4("view", view);
+}
+
+void Window::render() {
+    glClearColor(135 / 255.f, 206 / 255.f, 235 / 255.f, 1.f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    cubes->render();
 }
 
 void Window::frameBufferSizeCallback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
+    m_width = width;
+    m_height = height;
 }
--- src/Window.hpp
+++ src/Window.hpp
@@ -1,18 +1,21 @@
 #pragma once
 
+
 // clang-format off
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 // clang-format on
 
-#include "Shader.hpp"
-#include "Texture.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "Camera.hpp"
+#include "objects/Cube.hpp"
 #include <cmath>
 #include <print>
 #include <stb_image.h>
-
-typedef unsigned int uint;
-typedef unsigned char uchar;
+#include <sys/types.h>
 
 class Window {
 public:
@@ -23,19 +26,17 @@ public:
     void mainLoop();
 
 private:
-    const uint16_t m_width, m_height;
+    static glm::vec3 m_cameraPos, m_cameraFront, m_cameraUp;
+    static uint16_t m_width, m_height;
     const char* m_title;
     GLFWwindow* m_window;
-
-    // Vertex and Fragment Shader
-    Shader* shader;
-    Texture* tex;
-    uint VBO, VAO, EBO;
-
+    Shader* m_shader;
+    float m_deltaTime;
+    Cube* cubes;
 
     void initWindow();
-    void render();
     void update();
+    void render();
 
     static void frameBufferSizeCallback(GLFWwindow* window, int width, int height);
 };
