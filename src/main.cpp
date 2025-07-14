b9a4e39154309dd671e99b6a5a8568fe529e3645
ken85illo <pabillokenneth81@gmail.com>
2025-07-14 15:02:21 +0800

    chore: change window resolution

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Window.hpp>
 
 int main() {
-    Window::instance = new Window(960, 720, "Hello OpenGL!");
+    Window::instance = new Window(1280, 720, "ain't no way bruh");
     Window::instance->mainLoop();
     delete Window::instance;
 }
