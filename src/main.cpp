e3f6d8ef98937240a689a94402013e2d2bf9287a
ken85illo <pabillokenneth81@gmail.com>
2025-07-15 13:19:36 +0800

    feat: borderless fullscreen window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include <Window.hpp>
 
 int main() {
-    Window::instance = new Window(1280, 720, "ain't no way bruh");
+    if(!glfwInit()) {
+        std::println("Failed to initialize GLFW!");
+        return -1;
+    }
+
+    GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+    const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, false);
+    glfwWindowHint(GLFW_RED_BITS, mode->redBits);
+    glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
+    glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
+    glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
+
+    Window::instance = new Window(mode->width, mode->height, "ain't no way bruh", monitor);
     Window::instance->mainLoop();
     delete Window::instance;
 }
