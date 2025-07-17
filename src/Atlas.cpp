835c332bafcdffab36d30ff14c074e57240a8f9f
ken85illo <pabillokenneth81@gmail.com>
2025-07-17 23:15:01 +0800

    refactor: pass array and index to atlas instead of faces

--- src/Atlas.cpp
+++ src/Atlas.cpp
@@ -1,7 +1,13 @@
 #include "Atlas.hpp"
 
-Atlas::Atlas(uint16_t bitsPerTexture, uint16_t totalBits, float* vertices)
-: m_bitsPerTexture(bitsPerTexture), m_totalBits(totalBits), m_vertices(vertices) {
+Atlas::Atlas(uint16_t bitsPerTexture,
+uint16_t totalBits,
+glm::vec2 texCoords[6][4],
+uint16_t textures[][6])
+: m_bitsPerTexture(bitsPerTexture),
+  m_totalBits(totalBits),
+  m_texCoords(*texCoords),
+  m_textures(*textures) {
 }
 
 std::unordered_map<std::string, uint16_t> Atlas::getMap(uint16_t side) {
@@ -15,60 +21,61 @@ std::unordered_map<std::string, uint16_t> Atlas::getMap(uint16_t side) {
     return map;
 }
 
-void Atlas::map(uint16_t front, uint16_t back, uint16_t top, uint16_t bottom, uint16_t right, uint16_t left) {
-    auto mfront = getMap(front);
-    auto mback = getMap(back);
-    auto mtop = getMap(top);
-    auto mbottom = getMap(bottom);
-    auto mright = getMap(right);
-    auto mleft = getMap(left);
+void Atlas::map(uint16_t index) {
+    static uint16_t lastIndex = -1;
 
-    uint16_t texMap[] = {
+    if(index == lastIndex)
+        return;
+
+    lastIndex = index;
+
+    auto mfront = getMap(*(m_textures + index * 6));
+    auto mback = getMap(*(m_textures + index * 6 + 1));
+    auto mtop = getMap(*(m_textures + index * 6 + 2));
+    auto mbottom = getMap(*(m_textures + index * 6 + 3));
+    auto mright = getMap(*(m_textures + index * 6 + 4));
+    auto mleft = getMap(*(m_textures + index * 6 + 5));
+
+    glm::vec2 texMap[6][4] = {
         // front face
-        mfront["left"], mfront["bottom"],  // bottom-left
-        mfront["left"], mfront["top"],     // top-left
-        mfront["right"], mfront["top"],    // top-right
-        mfront["right"], mfront["bottom"], // bottom-right
+        glm::vec2(mfront["left"], mfront["bottom"]),  // bottom-left
+        glm::vec2(mfront["right"], mfront["bottom"]), // bottom-right
+        glm::vec2(mfront["right"], mfront["top"]),    // top-right
+        glm::vec2(mfront["left"], mfront["top"]),     // top-left
 
         // back face
-        mback["right"], mback["bottom"], // bottom-left
-        mback["right"], mback["top"],    // top-left
-        mback["left"], mback["top"],     // top-right
-        mback["left"], mback["bottom"],  // bottom-right
+        glm::vec2(mback["right"], mback["bottom"]), // bottom-left
+        glm::vec2(mback["left"], mback["bottom"]),  // bottom-right
+        glm::vec2(mback["left"], mback["top"]),     // top-right
+        glm::vec2(mback["right"], mback["top"]),    // top-left
 
         // top face
-        mtop["left"], mtop["bottom"],  // bottom-left
-        mtop["left"], mtop["top"],     // top-left
-        mtop["right"], mtop["top"],    // top-right
-        mtop["right"], mtop["bottom"], // bottom-right
+        glm::vec2(mtop["left"], mtop["bottom"]),  // bottom-left
+        glm::vec2(mtop["right"], mtop["bottom"]), // bottom-right
+        glm::vec2(mtop["right"], mtop["top"]),    // top-right
+        glm::vec2(mtop["left"], mtop["top"]),     // top-left
 
         // bottom face
-        mbottom["right"], mbottom["bottom"], // bottom-left
-        mbottom["right"], mbottom["top"],    // top-left
-        mbottom["left"], mbottom["top"],     // top-right
-        mbottom["left"], mbottom["bottom"],  // bottom-right
+        glm::vec2(mbottom["right"], mbottom["bottom"]), // bottom-left
+        glm::vec2(mbottom["left"], mbottom["bottom"]),  // bottom-right
+        glm::vec2(mbottom["left"], mbottom["top"]),     // top-right
+        glm::vec2(mbottom["right"], mbottom["top"]),    // top-left
 
         // right face
-        mright["left"], mright["bottom"],  // bottom-left
-        mright["left"], mright["top"],     // top-left
-        mright["right"], mright["top"],    // top-right
-        mright["right"], mright["bottom"], // bottom-right
+        glm::vec2(mright["left"], mright["bottom"]),  // bottom-left
+        glm::vec2(mright["right"], mright["bottom"]), // bottom-right
+        glm::vec2(mright["right"], mright["top"]),    // top-right
+        glm::vec2(mright["left"], mright["top"]),     // top-left
 
         // left face
-        mleft["right"], mleft["bottom"], // bottom-left
-        mleft["right"], mleft["top"],    // top-left
-        mleft["left"], mleft["top"],     // top-right
-        mleft["left"], mleft["bottom"],  // bottom-right
+        glm::vec2(mleft["right"], mleft["bottom"]), // bottom-left
+        glm::vec2(mleft["left"], mleft["bottom"]),  // bottom-right
+        glm::vec2(mleft["left"], mleft["top"]),     // top-right
+        glm::vec2(mleft["right"], mleft["top"]),    // top-left
     };
 
-    for(int i = 0, j = 3; i < std::size(texMap); i++, j += 5) {
-        int temp = j;
-        while(i < std::size(texMap) && j < temp + 2) {
-            *(m_vertices + j) = texMap[i] / static_cast<float>(m_totalBits);
-            i++;
-            j++;
-        }
-        i--;
-        j = temp;
+    for(int i = 0; i < 24; i++) {
+        (m_texCoords + i)->x = (*texMap + i)->x / static_cast<float>(m_totalBits);
+        (m_texCoords + i)->y = (*texMap + i)->y / static_cast<float>(m_totalBits);
     }
 }
--- src/Atlas.hpp
+++ src/Atlas.hpp
@@ -1,21 +1,25 @@
 #pragma once
 
+#include <array>
 #include <cstdint>
+#include <glm/glm.hpp>
 #include <string>
 #include <unordered_map>
 
-// convert variable name to string
-#define GET_VARIABLE_NAME(variable) (#variable)
-
 class Atlas {
 public:
-    Atlas(uint16_t bitsPerTexture, uint16_t totalBits, float* vertices);
-    void map(uint16_t front, uint16_t back, uint16_t top, uint16_t bottom, uint16_t right, uint16_t left);
+    Atlas(uint16_t bitsPerTexture,
+    uint16_t totalBits,
+    glm::vec2 texCoords[6][4],
+    uint16_t textures[][6]);
+
+    void map(uint16_t index);
 
 private:
     uint16_t m_bitsPerTexture;
     uint16_t m_totalBits;
-    float* m_vertices;
+    glm::vec2* m_texCoords;
+    uint16_t* m_textures;
 
     std::unordered_map<std::string, uint16_t> getMap(uint16_t side);
 };
