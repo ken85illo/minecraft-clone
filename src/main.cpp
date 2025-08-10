fdbd0f738693315816a18a8839b987e091d33857
ken85illo <pabillokenneth81@gmail.com>
2025-08-10 22:42:23 +0800

    refactor: move glfwInit to window class

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,6 @@
 #include "Core/Application.hpp"
 
 int main() {
-    if(!glfwInit()) {
-        std::println("Failed to initialize GLFW!");
-        return -1;
-    }
 
     Application app;
     app.mainLoop();
