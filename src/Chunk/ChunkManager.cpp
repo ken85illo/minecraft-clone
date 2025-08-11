3670a84170da47089f0c06ebd62f7dc273be5f49
ken85illo <pabillokenneth81@gmail.com>
2025-08-11 12:14:27 +0800

    fix: both meshes should be built when updating blocks

--- src/Chunk/ChunkManager.cpp
+++ src/Chunk/ChunkManager.cpp
@@ -10,21 +10,19 @@ void ChunkManager::uploadMesh(Chunk& chunk, MeshType meshType) {
     chunk.m_renderer.uploadMesh(chunk.m_meshData[meshType], meshType);
 }
 
-void ChunkManager::buildMesh(Chunk& chunk, const MeshData& data, MeshType meshType) {
-    chunk.m_meshData[meshType] = data;
-    chunk.m_renderer.uploadMesh(chunk.m_meshData[meshType], meshType);
+void ChunkManager::buildMesh(Chunk& chunk) {
+    updateMesh(chunk, ChunkMesh::buildOpaque(chunk), OPAQUE);
+    updateMesh(chunk, ChunkMesh::buildTransparent(chunk), TRANSPARENT);
+    uploadMesh(chunk, OPAQUE);
+    uploadMesh(chunk, TRANSPARENT);
 }
 
 void ChunkManager::updateBlock(Chunk& chunk, int32_t x, int32_t y, int32_t z, BlockType::Type blockType) {
     if(!chunk.m_blocks[x][y][z].isBreakable())
         return;
 
-    MeshType meshType = chunk.m_blocks[x][y][z].isTransparent() ? TRANSPARENT : OPAQUE;
-    MeshData (*generateMesh)(Chunk&) =
-    (meshType == 1) ? ChunkMesh::buildTransparent : ChunkMesh::buildOpaque;
-
     chunk.m_blocks[x][y][z].setType(blockType);
-    buildMesh(chunk, generateMesh(chunk), meshType);
+    buildMesh(chunk);
 
     chunk.setHighestBlock(y + 2);
     updateNeighbour(chunk.m_leftChunk, x == 0, CHUNK_SIZE - 1, y, z);
@@ -37,13 +35,7 @@ void ChunkManager::updateNeighbour(Chunk* neighbourChunk, bool condition, int32_
     if(!neighbourChunk || !condition || neighbourChunk->isAirBlock(x, y, z))
         return;
 
-    MeshType meshType =
-    neighbourChunk->m_blocks[x][y][z].isTransparent() ? TRANSPARENT : OPAQUE;
-
-    MeshData (*generateMesh)(Chunk&) =
-    (meshType == 1) ? ChunkMesh::buildTransparent : ChunkMesh::buildOpaque;
-
-    buildMesh(*neighbourChunk, generateMesh(*neighbourChunk), meshType);
+    buildMesh(*neighbourChunk);
 }
 
 void ChunkManager::deleteMesh(Chunk& chunk) {
--- src/Chunk/ChunkManager.hpp
+++ src/Chunk/ChunkManager.hpp
@@ -12,13 +12,13 @@ class ChunkManager {
 public:
     static void updateMesh(Chunk& chunk, const MeshData& data, MeshType meshType);
     static void uploadMesh(Chunk& chunk, MeshType meshType);
-    static void buildMesh(Chunk& chunk, const MeshData& data, MeshType meshType);
     static void
     updateBlock(Chunk& chunk, int32_t x, int32_t y, int32_t z, BlockType::Type blockType);
     static void deleteMesh(Chunk& chunk);
     static void render(Chunk& chunk, MeshType type);
 
 private:
+    static void buildMesh(Chunk& chunk);
     static void
     updateNeighbour(Chunk* neighbourChunk, bool condition, int32_t x, int32_t y, int32_t z);
 };
